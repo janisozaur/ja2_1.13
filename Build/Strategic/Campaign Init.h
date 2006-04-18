b7f1186a816d6916426862f3afbb904875144db2
lalien <lalien@3b4a5df2-a311-0410-b5c6-a8a6f20db521>
2006-04-18 19:08:25 +0000

    test
    git-svn-id: https://ja2svn.mooo.com/source/ja2/trunk@20 3b4a5df2-a311-0410-b5c6-a8a6f20db521

--- Build/Strategic/Campaign Init.h
+++
@@ -1,9 +0,0 @@
-#ifndef __CAMPAIGN_INIT_H
-#define __CAMPAIGN_INIT_H
-
-extern void InitNewCampaign();
-
-extern void BuildUndergroundSectorInfoList();
-extern void TrashUndergroundSectorInfo();
-
-#endif
