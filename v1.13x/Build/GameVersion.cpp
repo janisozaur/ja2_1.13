54b5aead8e5e40df22e086981d6f5f8ba452a9bb
lalien <lalien@3b4a5df2-a311-0410-b5c6-a8a6f20db521>
2006-05-21 14:08:28 +0000

    Little Alien
    - Game settings changed to read new Ja2_Options.INI format
    
    - Added control to skip strategic events 
    (set ENABLE_EMERGENCY_BUTTON_NUMLOCK_TO_SKIP_STRATEGIC_EVENTS to TRUE to enable, press and hold NumLock in strategy screen to skip strategic events)
    
    
    
    
    git-svn-id: https://ja2svn.mooo.com/source/ja2/trunk@77 3b4a5df2-a311-0410-b5c6-a8a6f20db521

--- v1.13x/Build/GameVersion.cpp
+++ v1.13x/Build/GameVersion.cpp
@@ -23,7 +23,7 @@ INT16		zVersionLabel[256]		= { L"Beta v. 0.98" };
 #else
 
 	//RELEASE BUILD VERSION 
-	INT16		zVersionLabel[256]		= { L"Release v1.13x.76 2006.05.21" };
+	INT16		zVersionLabel[256]		= { L"Release v1.13x.77 2006.05.21" };
 
 
 #endif
