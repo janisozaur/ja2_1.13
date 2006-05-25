b7d5a8bbf3333c0f7ef8e808742cbb176ba0e7df
lalien <lalien@3b4a5df2-a311-0410-b5c6-a8a6f20db521>
2006-05-25 11:29:27 +0000

    Little Alien
    - Game version string changed
    
    Added changes from Madd Mugsy 2006.05.24
    
    - Fixed tracer bullet firing during daytime bug
    - Made camobonus not depend on gear status
    - Items with coolness = 0 are "illegal"
    - Made wirecutter cursor appear for utility knife when able to cut fences
    - Made autofire-only guns not work if they're in the secondary hand
    
    
    
    git-svn-id: https://ja2svn.mooo.com/source/ja2/trunk@80 3b4a5df2-a311-0410-b5c6-a8a6f20db521

--- v1.13x/Build/GameVersion.cpp
+++ v1.13x/Build/GameVersion.cpp
@@ -23,12 +23,12 @@ INT16		zVersionLabel[256]		= { L"Beta v. 0.98" };
 #else
 
 	//RELEASE BUILD VERSION 
-	INT16		zVersionLabel[256]		= { L"Release v1.13x.79 2006.05.25" };
+	INT16		zVersionLabel[256]		= { L"Release v1.13x.80" };
 
 
 #endif
 
-INT8		czVersionNumber[16]	= { "Build 06.05.21" };
+INT8		czVersionNumber[16]	= { "Build 06.05.25" };
 INT16		zTrackingNumber[16]	= { L"Z" };
 	
 
