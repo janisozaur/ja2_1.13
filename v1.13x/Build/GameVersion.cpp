92ea2ee798a309eb716e9faf38eede1829cbdb9d
lalien <lalien@3b4a5df2-a311-0410-b5c6-a8a6f20db521>
2006-06-20 21:17:51 +0000

    - damage radius fixed
    git-svn-id: https://ja2svn.mooo.com/source/ja2/trunk@274 3b4a5df2-a311-0410-b5c6-a8a6f20db521

--- v1.13x/Build/GameVersion.cpp
+++ v1.13x/Build/GameVersion.cpp
@@ -23,7 +23,7 @@ INT16		zVersionLabel[256]		= { L"Beta v. 0.98" };
 #else
 
 	//RELEASE BUILD VERSION 
-	INT16		zVersionLabel[256]		= { L"Release v1.13x.273" };
+	INT16		zVersionLabel[256]		= { L"Release v1.13x.274" };
 
 
 #endif
