ba28e0d74eaa11b755cac10977f807121d3367dc
lalien <lalien@3b4a5df2-a311-0410-b5c6-a8a6f20db521>
2006-06-24 23:08:14 +0000

    - added more stats to soldier tool tip
    git-svn-id: https://ja2svn.mooo.com/source/ja2/trunk@298 3b4a5df2-a311-0410-b5c6-a8a6f20db521

--- v1.13x/Build/GameVersion.cpp
+++ v1.13x/Build/GameVersion.cpp
@@ -23,12 +23,12 @@ INT16		zVersionLabel[256]		= { L"Beta v. 0.98" };
 #else
 
 	//RELEASE BUILD VERSION 
-	INT16		zVersionLabel[256]		= { L"Release v1.13x.295" };
+	INT16		zVersionLabel[256]		= { L"Release v1.13x.298" };
 
 
 #endif
 
-INT8		czVersionNumber[16]	= { "Build 06.06.23" };
+INT8		czVersionNumber[16]	= { "Build 06.06.24" };
 INT16		zTrackingNumber[16]	= { L"Z" };
 	
 
