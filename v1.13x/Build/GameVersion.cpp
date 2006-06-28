d9fb425b9e0216cc0cb944301c92357bb1bc79b6
lalien <lalien@3b4a5df2-a311-0410-b5c6-a8a6f20db521>
2006-06-28 18:03:34 +0000

    - fixed: single shot AP's in weapon tool tip on auto fire only weapons not shown anymore
    git-svn-id: https://ja2svn.mooo.com/source/ja2/trunk@307 3b4a5df2-a311-0410-b5c6-a8a6f20db521

--- v1.13x/Build/GameVersion.cpp
+++ v1.13x/Build/GameVersion.cpp
@@ -23,12 +23,12 @@ INT16		zVersionLabel[256]		= { L"Beta v. 0.98" };
 #else
 
 	//RELEASE BUILD VERSION 
-	INT16		zVersionLabel[256]		= { L"Release v1.13x.298" };
+	INT16		zVersionLabel[256]		= { L"Release v1.13x.307" };
 
 
 #endif
 
-INT8		czVersionNumber[16]	= { "Build 06.06.24" };
+INT8		czVersionNumber[16]	= { "Build 06.06.28" };
 INT16		zTrackingNumber[16]	= { L"Z" };
 	
 
