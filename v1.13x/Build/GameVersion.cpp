a1c73d85d911fecaa91331a792e0ce625c527e6c
lalien <lalien@3b4a5df2-a311-0410-b5c6-a8a6f20db521>
2006-07-16 18:30:37 +0000

    - fix for HE ammo
    git-svn-id: https://ja2svn.mooo.com/source/ja2/trunk@347 3b4a5df2-a311-0410-b5c6-a8a6f20db521

--- v1.13x/Build/GameVersion.cpp
+++ v1.13x/Build/GameVersion.cpp
@@ -23,7 +23,7 @@ INT16		zVersionLabel[256]		= { L"Beta v. 0.98" };
 #else
 
 	//RELEASE BUILD VERSION 
-	INT16		zVersionLabel[256]		= { L"Release v1.13x.342" };
+	INT16		zVersionLabel[256]		= { L"Release v1.13x.347" };
 
 
 #endif
