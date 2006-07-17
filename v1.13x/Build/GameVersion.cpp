62f9e989e2ea746219e982d0f6b1c0d1b28f926f
lalien <lalien@3b4a5df2-a311-0410-b5c6-a8a6f20db521>
2006-07-17 16:39:48 +0000

    - fix for HE ammo - fix for militia reinforcements not working in cities
    git-svn-id: https://ja2svn.mooo.com/source/ja2/trunk@349 3b4a5df2-a311-0410-b5c6-a8a6f20db521

--- v1.13x/Build/GameVersion.cpp
+++ v1.13x/Build/GameVersion.cpp
@@ -23,12 +23,12 @@ INT16		zVersionLabel[256]		= { L"Beta v. 0.98" };
 #else
 
 	//RELEASE BUILD VERSION 
-	INT16		zVersionLabel[256]		= { L"Release v1.13x.347" };
+	INT16		zVersionLabel[256]		= { L"Release v1.13x.348" };
 
 
 #endif
 
-INT8		czVersionNumber[16]	= { "Build 06.07.09" };
+INT8		czVersionNumber[16]	= { "Build 06.07.17" };
 INT16		zTrackingNumber[16]	= { L"Z" };
 	
 
