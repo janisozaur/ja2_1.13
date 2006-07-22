eb6f6f5260843e695a6cb5f629c833f12a320f44
lalien <lalien@3b4a5df2-a311-0410-b5c6-a8a6f20db521>
2006-07-22 20:10:55 +0000

    - fixed the bug fix from build 365: Smoke grenade has the same effect as mustard gas grenade (need to run in gas cloud to reproduce)
    git-svn-id: https://ja2svn.mooo.com/source/ja2/trunk@367 3b4a5df2-a311-0410-b5c6-a8a6f20db521

--- v1.13x/Build/GameVersion.cpp
+++ v1.13x/Build/GameVersion.cpp
@@ -23,7 +23,7 @@ INT16		zVersionLabel[256]		= { L"Beta v. 0.98" };
 #else
 
 	//RELEASE BUILD VERSION 
-	INT16		zVersionLabel[256]		= { L"Release v1.13x.365" };
+	INT16		zVersionLabel[256]		= { L"Release v1.13x.367" };
 
 
 #endif
